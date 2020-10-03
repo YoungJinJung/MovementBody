ebc3ad584a8f86bdee19d679ed97b1d04d1f3c61
yj.jung <tlrpsh21@gmail.com>
2020-10-03 13:06:05 +0900

    fix(backend) : FoodCategory 수정
    통합영양성분정보 엑셀파일 기준으로 Category맞춤.
    해당 파일로 Food table DB 샘플 데이터 만드는 거 진행해야함.

--- src/main/java/com/project/movementbody/model/FoodCategory.java
+++ src/main/java/com/project/movementbody/model/FoodCategory.java
@@ -1,5 +1,8 @@
 package com.project.movementbody.model;
 
 public enum FoodCategory {
-    VEGETABLE, FRUIT, DRINK, MEAL, ETC
+    MEAL, //음식
+    INSTANT, //가공식품
+    SEAFOOD, //해산물
+    ARRIC//농축산물
 }
