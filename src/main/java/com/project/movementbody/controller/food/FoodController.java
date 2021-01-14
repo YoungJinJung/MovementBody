33a5966b1977a303e6d7f606a81487910e7669e0
jyj <youngjin_jung@tmax.co.kr>
2021-01-14 15:40:34 +0900

    fix(backend) : Swagger 적용안되었던 부분 추가

--- src/main/java/com/project/movementbody/controller/food/FoodController.java
+++ src/main/java/com/project/movementbody/controller/food/FoodController.java
@@ -2,6 +2,9 @@ package com.project.movementbody.controller.food;
 
 import com.project.movementbody.model.Food;
 import com.project.movementbody.service.food.FoodService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,27 +13,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
+@Api(value = "FoodController V1")
 public class FoodController {
+
     Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     FoodService foodService;
-
-    @PostMapping("/api/v1/foodInfo")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK !!"),
+            @ApiResponse(code = 500, message = "Internal Server Error !!"),
+            @ApiResponse(code = 404, message = "Not Found !!")
+    })
+    @PostMapping("/foodInfo")
     public Food addFoodInformation(@RequestBody Food food) {
         return foodService.create(food);
     }
 
-    @PutMapping("/api/v1/foodInfo")
+    @PutMapping("/foodInfo")
     public Food updateFoodInformation(@RequestBody Food food) {
         return foodService.update(food);
     }
 
-    @GetMapping("/api/v1/foodInfo/code/{foodCode}")
+    @GetMapping("/foodInfo/code/{foodCode}")
     public Food getFoodInformationByCode(@PathVariable String foodCode) {
         return foodService.readFoodByCode(foodCode);
     }
 
-    @GetMapping("/api/v1/foodInfo")
+    @GetMapping("/foodInfo")
     public List<Food> getFoodList() {
         List<Food> foodList = foodService.readAllFoodList();
         if (foodList == null || foodList.isEmpty()) {
@@ -39,7 +50,7 @@ public class FoodController {
         return foodList;
     }
 
-    @GetMapping("/api/v1/foodInfo/category/{foodCategory}")
+    @GetMapping("/foodInfo/category/{foodCategory}")
     public List<Food> getCategoryFoodList(@PathVariable String foodCategory) {
         List<Food> foodList = foodService.readFoodListByCategory(foodCategory);
         if (foodList == null || foodList.isEmpty()) {
@@ -48,12 +59,12 @@ public class FoodController {
         return foodList;
     }
 
-    @GetMapping("/api/v1/foodInfo/name/{foodName}")
+    @GetMapping("/foodInfo/name/{foodName}")
     public Food getFoodInformationByName(@PathVariable String foodName) {
         return foodService.readFoodByName(foodName);
     }
 
-    @DeleteMapping("/api/v1/foodInfo")
+    @DeleteMapping("/foodInfo")
     public boolean deleteFoodInformation(@RequestBody Food food) {
         return foodService.delete(food);
     }
