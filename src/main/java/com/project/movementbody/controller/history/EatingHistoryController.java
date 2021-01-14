778450b8b681ee40ad5717465f9fa740c645d9ef
jyj <youngjin_jung@tmax.co.kr>
2021-01-14 15:36:30 +0900

    fix(backend) : 누락된 파일 추가

---
+++ src/main/java/com/project/movementbody/controller/history/EatingHistoryController.java
@@ -0,0 +1,33 @@
+package com.project.movementbody.controller.history;
+
+import com.project.movementbody.model.EatingHistory;
+import com.project.movementbody.service.history.EatingHistoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EatingHistoryController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    EatingHistoryService eatingHistoryService;
+
+    @PostMapping("/api/v1/eatinghistory/add")
+    public boolean addEatingHistory(@RequestBody EatingHistory eatingHistory) {
+        return eatingHistoryService.insert(eatingHistory);
+    }
+
+    @GetMapping("/api/v1/eatinghistory/read/{userId}")
+    public List<EatingHistory> readEatingHistory(@PathVariable String userId){
+        return eatingHistoryService.selectByUserId(userId);
+    }
+
+    @PutMapping("/api/v1/eatinghistory/update")
+    public boolean updateEatingHistory(@RequestBody EatingHistory eatingHistory) {
+        return eatingHistoryService.insert(eatingHistory);
+    }
+
+}
