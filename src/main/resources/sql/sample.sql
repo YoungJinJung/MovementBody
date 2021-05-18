0ecdb4343d3d1e58d19647ebbd007607956b8526
jyj <youngjin_jung@tmax.co.kr>
2021-05-18 15:45:53 +0900

    fix(sql) : MySql용으로 수정

--- src/main/resources/sql/sample.sql
+++ src/main/resources/sql/sample.sql
@@ -1,455 +1,198 @@
 /* INSERT QUERY NO: 1 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
+INSERT INTO Food (foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
 VALUES
 (
-'D000006', 7.9, 1.7, 73.8, 0, '꿩불고기', 6.7, 252.9, 3.4
-);
-
-/* INSERT QUERY NO: 2 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000006', 7.9, 1.7, 73.8, 0, '꿩불고기', 6.7, 252.9, 3.4
+),
 (
-'D000007', 11.2, 6.5, 148.9, 0, '닭갈비', 11.5, 384, 5.3
-);
-
-/* INSERT QUERY NO: 3 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000007', 11.2, 6.5, 148.9, 0, '닭갈비', 11.5, 384, 5.3
+),
 (
-'D000008', 7.7, 10.5, 186.2, 0, '닭갈비', 15.2, 339, 2.8
-);
-
-/* INSERT QUERY NO: 4 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000008', 7.7, 10.5, 186.2, 0, '닭갈비', 15.2, 339, 2.8
+),
 (
-'D000009', 19.1, 12.2, 252.5, 0, '닭꼬치', 16.5, 409.9, 4.5
-);
-
-/* INSERT QUERY NO: 5 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000009', 19.1, 12.2, 252.5, 0, '닭꼬치', 16.5, 409.9, 4.5
+),
 (
-'D000010', 31.1, 5.2, 184, 0, '더덕구이', 3.1, 743.4, 11.6
-);
-
-/* INSERT QUERY NO: 6 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000010', 31.1, 5.2, 184, 0, '더덕구이', 3.1, 743.4, 11.6
+),
 (
-'D000011', 0.8, 6.6, 158.8, 0, '도미구이', 24.4, 632.9, 0
-);
-
-/* INSERT QUERY NO: 7 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000011', 0.8, 6.6, 158.8, 0, '도미구이', 24.4, 632.9, 0
+),
 (
-'D000012', 8.1, 14.4, 240.3, 0, '돼지갈비', 19.5, 404.7, 4.5
-);
-
-/* INSERT QUERY NO: 8 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000012', 8.1, 14.4, 240.3, 0, '돼지갈비', 19.5, 404.7, 4.5
+),
 (
-'D000015', 0.1, 12.3, 195.2, 0, '병어구이', 21.8, 627.2, 0
-);
-
-/* INSERT QUERY NO: 9 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000015', 0.1, 12.3, 195.2, 0, '병어구이', 21.8, 627.2, 0
+),
 (
-'D000016', 5.2, 16.8, 263.5, 0, '불고기', 22.9, 373.8, 5.3
-);
-
-/* INSERT QUERY NO: 10 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000016', 5.2, 16.8, 263.5, 0, '불고기', 22.9, 373.8, 5.3
+),
 (
-'D000017', 11.6, 16.2, 296.5, 0, '붕장어소금구이', 26, 124.8, 0.4
-);
-
-/* INSERT QUERY NO: 11 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000017', 11.6, 16.2, 296.5, 0, '붕장어소금구이', 26, 124.8, 0.4
+),
 (
-'D000020', 8.7, 23.9, 329.7, 0, '소양념갈비구이', 20, 459.5, 4.6
-);
-
-/* INSERT QUERY NO: 12 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000020', 8.7, 23.9, 329.7, 0, '소양념갈비구이', 20, 459.5, 4.6
+),
 (
-'D000021', 9.1, 23.5, 320.5, 0, '양념왕갈비', 18.1, 365.8, 4.5
-);
-
-/* INSERT QUERY NO: 13 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000021', 9.1, 23.5, 320.5, 0, '양념왕갈비', 18.1, 365.8, 4.5
+),
 (
-'D000022', 5.9, 20.4, 288.9, 0, '양념장어구이', 20.5, 469.8, 2.8
-);
-
-/* INSERT QUERY NO: 14 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000022', 5.9, 20.4, 288.9, 0, '양념장어구이', 20.5, 469.8, 2.8
+),
 (
-'D000023', 1.4, 11.8, 197.6, 0, '임연수구이', 21.5, 627.6, 0
-);
-
-/* INSERT QUERY NO: 15 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000023', 1.4, 11.8, 197.6, 0, '임연수구이', 21.5, 627.6, 0
+),
 (
-'D000024', 1.2, 8.1, 163.1, 0, '짚불구이곰장어', 21.4, 413.4, 0.6
-);
-
-/* INSERT QUERY NO: 16 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000024', 1.2, 8.1, 163.1, 0, '짚불구이곰장어', 21.4, 413.4, 0.6
+),
 (
-'D000025', 10.4, 14.8, 228.8, 0, '햄버그스테이크', 13.5, 475.1, 2.7
-);
-
-/* INSERT QUERY NO: 17 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000025', 10.4, 14.8, 228.8, 0, '햄버그스테이크', 13.5, 475.1, 2.7
+),
 (
-'D000026', 13.5, 7.7, 218.8, 0, '황태구이', 23.9, 567.7, 7.1
-);
-
-/* INSERT QUERY NO: 18 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000026', 13.5, 7.7, 218.8, 0, '황태구이', 23.9, 567.7, 7.1
+),
 (
-'D000027', 4.8, 25.6, 318.9, 0, '훈제오리', 17.2, 488.1, 0.1
-);
-
-/* INSERT QUERY NO: 19 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000027', 4.8, 25.6, 318.9, 0, '훈제오리', 17.2, 488.1, 0.1
+),
 (
-'D000028', 2.9, 1.1, 37.8, 0, '게국지', 4.1, 367.9, 1
-);
-
-/* INSERT QUERY NO: 20 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000028', 2.9, 1.1, 37.8, 0, '게국지', 4.1, 367.9, 1
+),
 (
-'D000029', 2.4, 1.2, 41.9, 0, '곰치국', 5.4, 380.2, 0.3
-);
-
-/* INSERT QUERY NO: 21 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000029', 2.4, 1.2, 41.9, 0, '곰치국', 5.4, 380.2, 0.3
+),
 (
-'D000030', 13.6, 2.7, 105.1, 0, '굴국밥', 6.5, 251.6, 0
-);
-
-/* INSERT QUERY NO: 22 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000030', 13.6, 2.7, 105.1, 0, '굴국밥', 6.5, 251.6, 0
+),
 (
-'D000032', 2.3, 0.6, 19.8, 0, '김치국', 1.2, 364.9, 0.6
-);
-
-/* INSERT QUERY NO: 23 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000032', 2.3, 0.6, 19.8, 0, '김치국', 1.2, 364.9, 0.6
+),
 (
-'D000036', 15.8, 1.6, 89.3, 0, '떡만둣국', 2.9, 282.9, 0
-);
-
-/* INSERT QUERY NO: 24 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000036', 15.8, 1.6, 89.3, 0, '떡만둣국', 2.9, 282.9, 0
+),
 (
-'D000037', 6.8, 2.5, 62, 0, '만둣국', 3.1, 338.3, 0.4
-);
-
-/* INSERT QUERY NO: 25 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000037', 6.8, 2.5, 62, 0, '만둣국', 3.1, 338.3, 0.4
+),
 (
-'D000038', 4.9, 0.8, 31.6, 0, '맑은감자국', 1.2, 244.6, 0.7
-);
-
-/* INSERT QUERY NO: 26 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'D000038', 4.9, 0.8, 31.6, 0, '맑은감자국', 1.2, 244.6, 0.7
+),
 (
-'R001795', 3.4, 7.4, 136, 3, '"달걀', 0, 12.4, 131
-);
-
-/* INSERT QUERY NO: 27 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001795', 3.4, 7.4, 136, 3, '"달걀', 0, 12.4, 131
+),
 (
-'R001796', 4.5, 0, 376, 3, '"달걀', 0, 82.4, 1238
-);
-
-/* INSERT QUERY NO: 28 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001796', 4.5, 0, 376, 3, '"달걀', 0, 82.4, 1238
+),
 (
-'R001797', 2.2, 8, 143, 3, '"달걀', 0, 13.9, 140
-);
-
-/* INSERT QUERY NO: 29 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001797', 2.2, 8, 143, 3, '"달걀', 0, 13.9, 140
+),
 (
-'R001798', 1, 0, 51, 3, '"달걀', 0, 0, 10.9
-);
-
-/* INSERT QUERY NO: 30 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001798', 1, 0, 51, 3, '"달걀', 0, 0, 10.9
+),
 (
-'R001799', 0, 0.1, 56, 3, '"달걀', 0, 0, 12.8
-);
-
-/* INSERT QUERY NO: 31 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001799', 0, 0.1, 56, 3, '"달걀', 0, 0, 12.8
+),
 (
-'R001800', 9.3, 23.5, 318, 3, '"달걀', 0, 0, 14.7
-);
-
-/* INSERT QUERY NO: 32 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001800', 9.3, 23.5, 318, 3, '"달걀', 0, 0, 14.7
+),
 (
-'R001801', 1.4, 32.2, 379, 3, '"달걀', 0, 0, 16.5
-);
-
-/* INSERT QUERY NO: 33 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001801', 1.4, 32.2, 379, 3, '"달걀', 0, 0, 16.5
+),
 (
-'R001805', 3.8, 13.5, 206, 3, '"달걀부침', 0, 15.1, 162
-);
-
-/* INSERT QUERY NO: 34 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001805', 3.8, 13.5, 206, 3, '"달걀부침', 0, 15.1, 162
+),
 (
-'R001806', 0.2, 11.7, 164, 3, '"수란', 0, 12.3, 110
-);
-
-/* INSERT QUERY NO: 35 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001806', 0.2, 11.7, 164, 3, '"수란', 0, 12.3, 110
+),
 (
-'R001807', 1.6, 11, 149, 3, '"스크램블에그', 0, 10, 145
-);
-
-/* INSERT QUERY NO: 36 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001807', 1.6, 11, 149, 3, '"스크램블에그', 0, 10, 145
+),
 (
-'R001808', 1.7, 9, 146, 3, '"메추리알', 0, 12.8, 137
-);
-
-/* INSERT QUERY NO: 37 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001808', 1.7, 9, 146, 3, '"메추리알', 0, 12.8, 137
+),
 (
-'R001809', 1.7, 10.1, 155, 3, '"메추리알', 0, 12.5, 94
-);
-
-/* INSERT QUERY NO: 38 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001809', 1.7, 10.1, 155, 3, '"메추리알', 0, 12.5, 94
+),
 (
-'R001810', 2, 11.3, 171, 3, '"오리알', 0, 13.1, 136
-);
-
-/* INSERT QUERY NO: 39 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001810', 2, 11.3, 171, 3, '"오리알', 0, 13.1, 136
+),
 (
-'R001811', 0.5, 12.2, 175, 3, '"피단', 0, 13.4, 850
-);
-
-/* INSERT QUERY NO: 40 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001811', 0.5, 12.2, 175, 3, '"피단', 0, 13.4, 850
+),
 (
-'R001812', 1, 12.3, 178, 3, '"청둥오리알', 0, 13.6, 0
-);
-
-/* INSERT QUERY NO: 41 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001812', 1, 12.3, 178, 3, '"청둥오리알', 0, 13.6, 0
+),
 (
-'R001813', 0.3, 1.8, 128, 3, '"가다랑어', 0, 25.9, 44
-);
-
-/* INSERT QUERY NO: 42 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001813', 0.3, 1.8, 128, 3, '"가다랑어', 0, 25.9, 44
+),
 (
-'R001814', 0.3, 4.8, 168, 3, '"가다랑어', 0, 29, 800
-);
-
-/* INSERT QUERY NO: 43 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001814', 0.3, 4.8, 168, 3, '"가다랑어', 0, 29, 800
+),
 (
-'R001815', 0, 0, 0, 3, '가다랑어 육수', 0, 0, 0
-);
-
-/* INSERT QUERY NO: 44 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001815', 0, 0, 0, 3, '가다랑어 육수', 0, 0, 0
+),
 (
-'R001816', 0.7, 1.3, 78, 3, '"가라지', 0, 14.8, 0
-);
-
-/* INSERT QUERY NO: 45 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'R001816', 0.7, 1.3, 78, 3, '"가라지', 0, 14.8, 0
+),
 (
-'P028615', 12.6, 0, 52.6, 1, '사과한모금 (190ml*24입)', 0, 0, 12.1
-);
-
-/* INSERT QUERY NO: 46 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028615', 12.6, 0, 52.6, 1, '사과한모금 (190ml*24입)', 0, 0, 12.1
+),
 (
-'P028616', 12.5, 0, 50, 1, '사과한모금 (120ml*16입)', 0, 0, 11.7
-);
-
-/* INSERT QUERY NO: 47 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028616', 12.5, 0, 50, 1, '사과한모금 (120ml*16입)', 0, 0, 11.7
+),
 (
-'P028617', 14, 0, 60, 1, '생생비타 (100ml*10포)', 0, 0, 11
-);
-
-/* INSERT QUERY NO: 48 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028617', 14, 0, 60, 1, '생생비타 (100ml*10포)', 0, 0, 11
+),
 (
-'P028618', 14, 0, 60, 1, '생생비타 100ml', 0, 0, 11
-);
-
-/* INSERT QUERY NO: 49 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028618', 14, 0, 60, 1, '생생비타 100ml', 0, 0, 11
+),
 (
-'P028619', 11.7, 0, 50, 1, '석류즙 (120ml*15포)', 0.8, 0, 10.8
-);
-
-/* INSERT QUERY NO: 50 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028619', 11.7, 0, 50, 1, '석류즙 (120ml*15포)', 0.8, 0, 10.8
+),
 (
-'P028620', 9.2, 0, 37.5, 1, '숨 (120ml*24입)', 0, 0, 6.7
-);
-
-/* INSERT QUERY NO: 51 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028620', 9.2, 0, 37.5, 1, '숨 (120ml*24입)', 0, 0, 6.7
+),
 (
-'P028621', 8.9, 0, 36.8, 1, '숨 (190ml*24입 )', 0, 2.6, 6.3
-);
-
-/* INSERT QUERY NO: 52 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028621', 8.9, 0, 36.8, 1, '숨 (190ml*24입 )', 0, 2.6, 6.3
+),
 (
-'P028622', 8.9, 0, 36.8, 1, '숨 190ml', 0, 2.6, 6.3
-);
-
-/* INSERT QUERY NO: 53 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028622', 8.9, 0, 36.8, 1, '숨 190ml', 0, 2.6, 6.3
+),
 (
-'P028623', 9.2, 0, 37.5, 1, '숨 120ml', 0, 0, 6.7
-);
-
-/* INSERT QUERY NO: 54 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028623', 9.2, 0, 37.5, 1, '숨 120ml', 0, 0, 6.7
+),
 (
-'P028624', 11, 0, 45, 1, '유기배즙 100ml', 0, 0, 8
-);
-
-/* INSERT QUERY NO: 55 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028624', 11, 0, 45, 1, '유기배즙 100ml', 0, 0, 8
+),
 (
-'P028625', 12.4, 0, 50.5, 1, '자연드림감귤주스 1L', 0.2, 1, 11.4
-);
-
-/* INSERT QUERY NO: 56 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028625', 12.4, 0, 50.5, 1, '자연드림감귤주스 1L', 0.2, 1, 11.4
+),
 (
-'P028626', 12.4, 0, 50.5, 1, '자연드림감귤주스 (1L*12개입)', 0, 1, 11.4
-);
-
-/* INSERT QUERY NO: 57 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028626', 12.4, 0, 50.5, 1, '자연드림감귤주스 (1L*12개입)', 0, 1, 11.4
+),
 (
-'P028627', 11.7, 0, 50, 1, '토마토당근한모금(120ml*24입)', 0, 8.3, 11.7
-);
-
-/* INSERT QUERY NO: 58 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028627', 11.7, 0, 50, 1, '토마토당근한모금(120ml*24입)', 0, 8.3, 11.7
+),
 (
-'P028628', 11.7, 0, 50, 1, '토마토당근한모금(120ml*16입)', 0, 8.3, 11.7
-);
-
-/* INSERT QUERY NO: 59 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028628', 11.7, 0, 50, 1, '토마토당근한모금(120ml*16입)', 0, 8.3, 11.7
+),
 (
-'P028629', 11.7, 0, 50, 1, '토마토당근한모금 120ml', 0, 8.3, 11.7
-);
-
-/* INSERT QUERY NO: 60 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028629', 11.7, 0, 50, 1, '토마토당근한모금 120ml', 0, 8.3, 11.7
+),
 (
-'P028630', 12.8, 0, 52.2, 1, '포도주스 180ml', 0, 5.6, 10
-);
-
-/* INSERT QUERY NO: 61 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028630', 12.8, 0, 52.2, 1, '포도주스 180ml', 0, 5.6, 10
+),
 (
-'P028631', 12.5, 0, 54.2, 1, '포도주스 (180ml*10개)', 0, 4.2, 10
-);
-
-/* INSERT QUERY NO: 62 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028631', 12.5, 0, 54.2, 1, '포도주스 (180ml*10개)', 0, 4.2, 10
+),
 (
-'P028632', 12.5, 0, 54.2, 1, '포도즙 (120ml*20포)', 0.6, 0, 10.8
-);
-
-/* INSERT QUERY NO: 63 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'P028632', 12.5, 0, 54.2, 1, '포도즙 (120ml*20포)', 0.6, 0, 10.8
+),
 (
-'N000581', 0.3, 0.1, 83, 2, '쥐치', 19.1, 0, 0
-);
-
-/* INSERT QUERY NO: 64 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'N000581', 0.3, 0.1, 83, 2, '쥐치', 19.1, 0, 0
+),
 (
-'N000570', 1.1, 0.3, 87, 2, '주름송편게', 18.8, 0, 0
-);
-
-/* INSERT QUERY NO: 65 */
-INSERT INTO food(foodCode, carbon, fat, foodCalorie, foodCategory, foodName, protein, sodium, sugar)
-VALUES
+    'N000570', 1.1, 0.3, 87, 2, '주름송편게', 18.8, 0, 0
+),
 (
-'N000573', 0.2, 0.2, 76, 2, '줄가자미', 17.4, 0, 0
+    'N000573', 0.2, 0.2, 76, 2, '줄가자미', 17.4, 0, 0
 );
-
